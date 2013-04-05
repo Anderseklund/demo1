780fcab59682813259907983fe214ee857db5606
Anders Eklund <Anders@Anders-MacBook-Pro.local>
2013-04-05 11:06:24 +0200

    Initial Commit

---
+++ demo1/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  demo1
+//
+//  Created by Anders Eklund on 2013-04-05.
+//  Copyright (c) 2013 Anders. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
