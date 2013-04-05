a1d1e715dd5231c8a30382d5e665da53ef06cd46
Anders Eklund <Anders@Anders-MacBook-Pro.local>
2013-04-05 11:09:41 +0200

    Added project

---
+++ DemoFile.h
@@ -0,0 +1,13 @@
+//
+//  DemoFile.h
+//  demo1
+//
+//  Created by Anders Eklund on 2013-04-05.
+//  Copyright (c) 2013 Anders. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DemoFile : NSObject
+
+@end
---
+++ DemoFile.m
@@ -0,0 +1,13 @@
+//
+//  DemoFile.m
+//  demo1
+//
+//  Created by Anders Eklund on 2013-04-05.
+//  Copyright (c) 2013 Anders. All rights reserved.
+//
+
+#import "DemoFile.h"
+
+@implementation DemoFile
+
+@end
